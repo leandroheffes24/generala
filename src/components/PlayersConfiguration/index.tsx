529ed37c2531ef81d4681601f7a1d72e76f912f6
Leandro Heffes <leandroheffes@outlook.es>
2024-05-04 11:43:15 -0300

    corrections

--- src/components/PlayersConfiguration/index.tsx
+++ src/components/PlayersConfiguration/index.tsx
@@ -38,8 +38,8 @@ const PlayersConfiguration: React.FC<PlayersConfigurationProps> = ({changeCompon
         }
     }
 
-    const handleDeletePlayer = () => {
-
+    const handleDeletePlayer = (playerName: string) => {
+        
     }
 
     const renderPlayers = () => {
@@ -53,7 +53,7 @@ const PlayersConfiguration: React.FC<PlayersConfigurationProps> = ({changeCompon
                             {player.name}
                         </span>
 
-                        <button className={styles.deletePlayerButton(player.name)} onClick={handleDeletePlayer}>
+                        <button className={styles.deletePlayerButton} onClick={handleDeletePlayer(player.name)}>
                             <svg viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className={styles.removePlayerIcon}><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 7h16" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /><path d="M10 12l4 4m0 -4l-4 4" /></svg>
                         </button>
                     </li>
